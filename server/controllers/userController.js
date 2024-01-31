758999314d483fb7252a2a241bcbbe76a7c9eb47
rencdr <demirerencan@outlook.com>
2024-01-31 20:30:01 +0300

    password hashing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,16 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user.js');
 
 // Yeni bir kullanıcı eklemek
 exports.createUser = async (req, res) => {
   try {
+    // Kullanıcının şifresini bcrypt kullanarak şifrele
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword, // Şifreyi hashlenmiş şifre ile değiştir
     });
 
     await newUser.save();
