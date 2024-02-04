c5909562ec953b6e12e38fa4083ec1ddeecf5224
rencdr <demirerencan@outlook.com>
2024-02-04 16:29:46 +0300

    create new post frontend

--- client/src/components/CreatePostCard.jsx
+++ client/src/components/CreatePostCard.jsx
@@ -1,19 +1,20 @@
+// CreatePostCard.js
 import React, { useState } from 'react';
 import useCreatePost from '../hooks/useCreatePost';
 
 const CreatePostCard = () => {
-  const { posts, loading, error, createPost } = useCreatePost();
+  const { loading, error, createPost } = useCreatePost();
   const [newPost, setNewPost] = useState('');
 
   const handleCreatePost = async () => {
     try {
-      await createPost(newPost);
+      await createPost({ content: newPost });
 
       // Gönderi başarıyla oluşturulduğunda işlemler burada yapılabilir
       console.log('Post created successfully');
     } catch (error) {
       // Hata durumunda yapılacak işlemler burada yapılabilir
-      console.error('Error creating post:', error.response.data);
+      console.error('Error creating post:', error);
     }
   };
 
@@ -30,14 +31,6 @@ const CreatePostCard = () => {
         <button onClick={handleCreatePost} className="bg-blue-500 text-white p-2 rounded-md w-full">Create Post</button>
         {loading && <p>Loading...</p>}
         {error && <p className="text-red-500 mt-2">{error}</p>}
-        <div className="mt-4">
-          <h3 className="text-xl font-semibold mb-2">Existing Posts</h3>
-          {posts.map((post) => (
-            <div key={post.id} className="border p-2 mb-2 rounded-md">
-              {post.title}
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   );
--- client/src/hooks/useCreatePost.js
+++ client/src/hooks/useCreatePost.js
@@ -1,27 +1,24 @@
-import { useState, useEffect } from "react";
+// useCreatePost.js
+import { useState } from "react";
 import axios from "axios";
 
 const useCreatePost = () => {
-  const [posts, setPosts] = useState([]); 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchCreatePosts = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/api/post');
-        setPosts(response.data); 
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const createPost = async (newPost) => {
+    try {
+      setLoading(true);
+      const response = await axios.post('http://localhost:5000/api/post', newPost);
+      console.log(response.data); // Oluşturulan post hakkındaki bilgileri burada işleyebilirsiniz.
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    fetchCreatePosts();
-  }, []);
-
-  return { posts, loading, error }; 
+  return { loading, error, createPost };
 };
 
 export default useCreatePost;
